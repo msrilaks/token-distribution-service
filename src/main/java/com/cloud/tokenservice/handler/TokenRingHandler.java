e351a8d8a091cad913826bb7e0c7bc3f56b293f6
Srilakshmi <38964732+msrilaks@users.noreply.github.com>
2019-04-11 16:56:11 -0700

    Token ring algorithm

--- src/main/java/com/cloud/tokenservice/handler/TokenRingHandler.java
+++ src/main/java/com/cloud/tokenservice/handler/TokenRingHandler.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -15,23 +16,69 @@ public class TokenRingHandler {
 
     public TokenRing createTokenRing(Distribution distribution) {
         TokenRing tokenRing = new TokenRing();
-        Map<String, Integer> distMap = distribution.getAsMap();
-
+        Map<String, Integer> distMap = getDistributionAsMap(distribution);
+        Map<String, Integer> distributionWeightMap = getDistributionWeightMap(
+                distribution);
         for (int i = 0; i < 10; i++) {
-            distMap.keySet()
-                   .iterator()
-                   .forEachRemaining(token -> {
-                       int tokenCount = distMap.get(token) % 10;
-                       tokenRing.add(token, tokenCount);
-                   });
-
+            addTokens(distribution, tokenRing, distMap, distributionWeightMap);
         }
         LOGGER.info(
                 "Token Ring constructed for " + distribution.getId() + " : " +
                 tokenRing);
-
         return tokenRing;
     }
 
+    private void addTokens(
+            Distribution distribution, TokenRing tokenRing,
+            Map<String, Integer> distMap,
+            Map<String, Integer> distributionWeightMap) {
 
+        distMap.keySet()
+               .iterator()
+               .forEachRemaining(token -> {
+                   int tokenCount = distMap.get(token) / 10;
+                   int weight = distMap.get(token) % 10;
+                   int oldWeight = distributionWeightMap.get(token);
+                   if (weight + oldWeight >= 10) {
+                       LOGGER.info("Token Ring Distribution: " +
+                                   distribution.getId() + " token: " + token +
+                                   " weight: " + weight + " oldWeight:" +
+                                   oldWeight);
+                       distributionWeightMap.put(token,
+                                                 (weight + oldWeight) - 10);
+                       tokenCount++;
+                   } else {
+                       LOGGER.info("Token Ring Distribution: " +
+                                   distribution.getId() + " token: " + token +
+                                   " weight: " + weight + " oldWeight:" +
+                                   oldWeight);
+                       distributionWeightMap.put(token, (weight + oldWeight));
+                   }
+                   tokenRing.add(token, tokenCount);
+
+                   LOGGER.info(
+                           "Token Ring Distribution: " + distribution.getId() +
+                           " token: " + token + "tokenCount: " + tokenCount);
+               });
+    }
+
+    private Map<String, Integer> getDistributionAsMap(
+            Distribution distribution) {
+        Map<String, Integer> distMap = new HashMap<>();
+        distribution.getGroups()
+                    .forEach(group -> {
+                        distMap.put(group.getToken(), group.getPercentage());
+                    });
+        return distMap;
+    }
+
+    private Map<String, Integer> getDistributionWeightMap(
+            Distribution distribution) {
+        Map<String, Integer> distWeightMap = new HashMap<>();
+        distribution.getGroups()
+                    .forEach(group -> {
+                        distWeightMap.put(group.getToken(), 0);
+                    });
+        return distWeightMap;
+    }
 }
--- src/main/java/com/cloud/tokenservice/model/Distribution.java
+++ src/main/java/com/cloud/tokenservice/model/Distribution.java
@@ -5,9 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @Getter
@@ -18,11 +16,4 @@ public class Distribution implements Serializable {
     private String      name;
     private List<Group> groups;
 
-    public Map<String, Integer> getAsMap() {
-        Map<String, Integer> distMap = new HashMap<>();
-        groups.forEach(group -> {
-            distMap.put(group.getToken(), group.getPercentage());
-        });
-        return distMap;
-    }
 }
